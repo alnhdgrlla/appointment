4718d28bc4267bce4ddc4eae189f15b79a258fc5
alnhdgrlla <y.tanaka@shippio.io>
2019-11-05 10:44:56 +0900

    validation added

--- app/graphql/mutations/delete_examination.rb
+++ app/graphql/mutations/delete_examination.rb
@@ -17,6 +17,30 @@
 #   end
 # end
 
+# module Mutations
+#   class DeleteExamination < BaseMutation
+#     argument :examination_id, ID, required: true
+
+#     type Types::ExaminationType
+
+#     def resolve(examination_id:)
+#       examination  = Examination.find_by(id:examination_id)
+#       if examination.present?
+#         if context[:current_user].admin?
+#           examination.destroy
+#         elsif examination.user_id != context[:current_user].id
+#           examination.destroy
+#         else
+#           return GraphQL::ExecutionError.new("This is not your examination")
+#         end
+#         examination
+#       else
+#        return GraphQL::ExecutionError.new("This examination does not exist")
+#       end
+#     end
+#   end
+# end
+
 module Mutations
   class DeleteExamination < BaseMutation
     argument :examination_id, ID, required: true
@@ -24,11 +48,9 @@ module Mutations
     type Types::ExaminationType
 
     def resolve(examination_id:)
-      examination  = Examination.find_by(id:examination_id)
+      examination  = Examination.find_by(id: examination_id)
       if examination.present?
-        if context[:current_user].admin?
-          examination.destroy
-        elsif examination.user_id != context[:current_user].id
+        if context[:current_user].id == examination.doctor.id || examination.user_id == context[:current_user].id
           examination.destroy
         else
           return GraphQL::ExecutionError.new("This is not your examination")
--- app/graphql/mutations/update_examination.rb
+++ app/graphql/mutations/update_examination.rb
@@ -22,6 +22,53 @@
 #   end
 # end
 
+# module Mutations
+#   class UpdateExamination < BaseMutation
+
+#     argument :id, ID, required: true
+#     argument :data, Types::ExaminationInput, required: true
+
+#     type Types::ExaminationType
+
+#     def resolve(id:, data:)
+#       return GraphQL::ExecutionError.new("Forbidden action") if !context[:current_user].admin?
+#       exam = Examination.find_by(id: id)
+#       if !data[:doctor_id] 
+#         exam.update!(doctor: context[:current_user])
+#       else 
+#         exam.update!(doctor: User.find_by(id: data[:doctor_id]))
+#       end
+#       exam.update!(data.to_h)
+#       exam
+#     end
+#   end
+# end
+
+# module Mutations
+#   class UpdateExamination < BaseMutation
+
+#     argument :id, ID, required: true
+#     argument :data, Types::ExaminationInput, required: true
+
+#     type Types::ExaminationType
+
+#     def resolve(id:, data:)
+#       c_user = context[:current_user]
+#       exam = Examination.find_by(id: id)
+      
+#       return GraphQL::ExecutionError.new("Forbidden action") if !c_user.admin? && c_user.id != exam.doctor_id
+      
+#       if !data[:doctor_id] 
+#         exam.update!(doctor: context[:current_user])
+#       else 
+#         exam.update!(doctor: User.find_by(id: data[:doctor_id]))
+#       end
+#       exam.update!(data.to_h)
+#       exam
+#     end
+#   end
+# end
+
 module Mutations
   class UpdateExamination < BaseMutation
 
@@ -31,14 +78,20 @@ module Mutations
     type Types::ExaminationType
 
     def resolve(id:, data:)
-      return GraphQL::ExecutionError.new("Forbidden action") if !context[:current_user].admin?
+      c_user = context[:current_user]
+      
+      return GraphQL::ExecutionError.new("Forbidden action") if !c_user.admin?
+      
       exam = Examination.find_by(id: id)
-      if !data[:doctor_id] 
-        exam.update!(doctor: context[:current_user])
-      else 
-        exam.update!(doctor: User.find_by(id: data[:doctor_id]))
+      info = { status: data[:status], start_time: data[:start_time], end_time: data[:end_time], doctor:data[:doctor] }
+      info.reject!{|key,value| value == nil}
+      info[:doctor] = context[:current_user] if !info[:doctor]
+      if c_user.id = exam.doctor_id
+        info[:doctor] = User.find_by(id: data[:doctor_id]) if info[:doctor]
+      else
+        return GraphQL::ExecutionError.new("Forbidden action")
       end
-      exam.update!(data.to_h)
+      exam.update!(info)
       exam
     end
   end
